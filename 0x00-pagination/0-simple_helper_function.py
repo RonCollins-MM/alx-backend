459989743b227aff3d9a97f21ed8bf83b4a6ad5d
RonCollins-MM <muriukiron@gmail.com>
2023-04-16 18:27:27 +0300

    add simple pagination algorithm

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 """
-function named index_range that takes two integer arguments page and page_size
+Implementing pagination in a light-weight function
 """
 
 
 def index_range(page, page_size):
+    """Method that implements a simple pagination algorithm.
+
+    Parameters
+    ----------
+    page : int
+        The logical 'page' of data that is requested
+
+    page_size : int
+        The size of each virtual page
+
+    Returns
+    -------
+    tuple
+        A tuple of size 2 containing the start index and end
     """
-    function named index_range that takes two integer arguments:
-    page and page_size.
-    The function should return a tuple of size two containing:
-    a start index and an end index corresponding to
-    the range of indexes to return in a list
-    for those particular pagination parameters.
-    Page numbers are 1-indexed, i.e. the first page is page 1.
-    """
-    previous = (page - 1) * page_size
-    return (previous, previous + page_size)
+
+    end_index = page_size * page
+    start_index = end_index - page_size
+    
+    return (start_index,  + end_index)
