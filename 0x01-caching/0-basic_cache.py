310429bf95ae1706f48613c4c0aedd80989bb11f
RonCollins-MM <muriukiron@gmail.com>
2023-04-20 05:37:59 +0300

    Taks 0: simple caching algorithm

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -12,11 +12,9 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         if key is None or item is None:
             return
-
         self.cache_data.update({key: item})
 
     def get(self, key):
         if key is None or key not in self.cache_data.keys():
             return None
-        
         return self.cache_data[key]
